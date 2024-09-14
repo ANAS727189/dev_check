6cda3482cb252780d73e566f98587d779d179728
phantomsoldierking <patilyogeshgouda@gmail.com>
2024-09-14 15:42:47 +0530

    3qwe

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -8,28 +8,24 @@ interface CardProps {
 
 function Card({ title, text }: CardProps) {
   return (
-
     <div className="card w-full mt-36">
       <div className="flex flex-col items-center gap-12 py-36">
         <span className="icon">
           <Swords />
         </span>
-        <div className="ml-4">
-
-          <h4>{title}</h4>
-          <p>{text}</p>
+        
+        <div className=" flex flex-col gap-6 ml-4 text-2xl">
+            <h4>{title}</h4>
+            <p>{text}</p>
         </div>
       </div>
-      <div className="shine "></div>
-      <div className="background ">
-        <div className="tiles ">
-          {[...Array(10)].map((_, i) => (
-            <div key={i} className={`tile tile-${i + 1}`}></div>
-          ))}
-        </div>
-        <div className="line line-1"></div>
-        <div className="line line-2"></div>
-        <div className="line line-3"></div>
+      <div className="shine"></div>
+      <div className="background">
+        
+          <div className="line line-1"></div>
+          <div className="line line-2"></div>
+          <div className="line line-3"></div>
+
       </div>
     </div>
   );
